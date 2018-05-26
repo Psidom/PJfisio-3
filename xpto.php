28c84f81ceee6e32237bb68ad8eb50dcb6e0b3ce
Cristian Tinôco Tosta dos Reis <30469556+Psidom@users.noreply.github.com>
2018-05-26 11:39:41 -0300

    Teste de Adicao de conexao com funcao

--- xpto.php
+++ xpto.php
@@ -1,54 +1,18 @@
 <?php
+require 'conexao.php';
+
 $nome = isset($_POST["input1"]) ?  $_POST["input1"] : "";
 $email = isset($_POST["input2"]) ? $_POST["input2"] : "";
 $plano = isset($_POST["input3"]) ? $_POST["input3"] : "";
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "academia"; 
+/* @var $nome type */
+$nome = filter_var($nome,FILTER_SANITIZE_STRING);
 
-// Criar conexão
-$conn = new mysqli($servername, $username, $password);
+$email = filter_var($email,FILTER_SANITIZE_EMAIL);
+/* @var $filter_var type */
+$plano = filter_var($plano,FILTER_SANITIZE_STRING);
 
-// Checar conexão
-$conn->connect_error;
-
-// Create database
-$sql = "CREATE DATABASE IF NOT EXISTS academia";
-$conn->query($sql);
-
-// sql to create table
-$sql = "CREATE TABLE IF NOT EXISTS solicitacoes (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    nome VARCHAR(30) NOT NULL,
-    plano VARCHAR(300) NOT NULL,
-    email VARCHAR(50)
-    )";
-    
-    $conn->query($sql);
-    $conn->close();
-
-
-$conn = new mysqli($servername, $username, $password,$dbname);
-//preparando o bind
-$stmt= $conn->prepare("INSERT INTO solicitacoes (nome,plano,email) VALUES (?,?,?)");
-$stmt->bind_param("sss",$rnome, $rplano, $remail);
-
-//Setar Parametros e executar
-$rnome = $nome;
-$rplano= $plano;
-$remail = $email;
-
-if ($stmt->execute() === TRUE) {
-    //echo "Solicitação feita com sucesso";
-    echo"<script>alert('Solicitação feita com sucesso')</script>";
-} else {
-    echo"<script>alert('Error: ' . $sql . '<br>' . $conn->error)</script>";
-    //echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-$conn->close();
+inserir($nome,$email,$plano);
 
 /* $para="psidom@gmail.com";
 $titulo= "Plano para Academia";
